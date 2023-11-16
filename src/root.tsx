11b17f77623d7930fbf71c081cb4315bf6f264b8
Dogan Öztürk <doganozturk2005@gmail.com>
2023-11-16 15:48:26 +0300

    fixes problematic script definition on head.

--- src/root.tsx
+++ src/root.tsx
@@ -51,13 +51,7 @@ export default component$(() => {
         <meta name="theme-color" content="#1a2334" />
         <meta name="apple-mobile-web-app-status-bar-style" content="#1a2334" />
 
-        <script type="text/javascript">
-          (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "jqur0l1wt3");
-        </script>
+        <script dangerouslySetInnerHTML={`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "jqur0l1wt3");`}/>
 
         <RouterHead />
       </head>
