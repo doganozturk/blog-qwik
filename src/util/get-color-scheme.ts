ba6e38e29031abf0d45e5495c2e2984c19a9a536
jonathan <bcbg92@hotmail.com>
2025-06-05 00:22:29 +0200

    improve the get-color-scheme

--- src/util/get-color-scheme.ts
+++ src/util/get-color-scheme.ts
@@ -1,30 +1,57 @@
 import { isServer } from "@builder.io/qwik/build";
 
-export enum ColorScheme {
-  Dark = "dark",
-  Light = "light",
-  NoPreference = "no-preference",
-}
+export const ColorScheme = {
+	Dark: "dark",
+	Light: "light",
+	NoPreference: "no-preference",
+} as const;
 
-export const getColorScheme = (): ColorScheme => {
-  if (isServer) {
-    return ColorScheme.Light;
-  }
+export type ColorSchemeType = (typeof ColorScheme)[keyof typeof ColorScheme];
 
-  if (!window.matchMedia) {
-    return ColorScheme.NoPreference;
-  }
+const LOCAL_STORAGE_KEY = "user-color-scheme";
 
-  // Check for dark mode preference
-  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    return ColorScheme.Dark;
-  }
+export const getColorScheme = (): ColorSchemeType => {
+	// Si le code est exécuté côté serveur, retourner Light par défaut
+	if (isServer) {
+		return ColorScheme.Light;
+	}
 
-  // Check for light mode preference
-  if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-    return ColorScheme.Light;
-  }
+	// Vérifier si window et localStorage sont disponibles
+	if (
+		typeof window === "undefined" ||
+		!window.localStorage ||
+		!window.matchMedia
+	) {
+		return ColorScheme.NoPreference;
+	}
 
-  // No preference
-  return ColorScheme.NoPreference;
+	// Vérifier si une préférence est déjà sauvegardée dans le localStorage
+	const savedPreference = window.localStorage.getItem(
+		LOCAL_STORAGE_KEY,
+	) as ColorSchemeType | null;
+	if (savedPreference) {
+		return savedPreference;
+	}
+
+	// Vérifier la préférence de schéma de couleurs
+	const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+	const lightModeQuery = window.matchMedia("(prefers-color-scheme: light)");
+
+	let preference: ColorSchemeType;
+
+	switch (true) {
+		case darkModeQuery.matches:
+			preference = ColorScheme.Dark;
+			break;
+		case lightModeQuery.matches:
+			preference = ColorScheme.Light;
+			break;
+		default:
+			preference = ColorScheme.NoPreference;
+	}
+
+	// Sauvegarder la préférence dans le localStorage
+	window.localStorage.setItem(LOCAL_STORAGE_KEY, preference);
+
+	return preference;
 };
