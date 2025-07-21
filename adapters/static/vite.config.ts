d5394323c8360fedacc4d78a706b2d50fcbb50e8
Doğan Öztürk <dogan.ozturk@trendyol.com>
2025-07-21 09:30:39 +0300

    fix: enable static site generation for proper HTML output

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -1,9 +1,6 @@
-import { qwikVite } from "@builder.io/qwik/optimizer";
+import { staticAdapter } from "@builder.io/qwik-city/adapters/static/vite";
 import { extendConfig } from "@builder.io/qwik-city/vite";
-import { qwikCity } from "@builder.io/qwik-city/vite";
-import { generate } from "@builder.io/qwik-city/static";
 import baseConfig from "../../vite.config";
-import path from "node:path";
 
 export default extendConfig(baseConfig, () => {
   return {
@@ -14,22 +11,9 @@ export default extendConfig(baseConfig, () => {
       },
     },
     plugins: [
-      qwikCity(),
-      qwikVite(),
-      {
-        name: "static-generate",
-        closeBundle: async () => {
-          await generate({
-            origin: "http://doganozturk.dev",
-            outDir: path.resolve(__dirname, "./dist"),
-            renderModulePath: path.resolve(
-              __dirname,
-              "../../dist/entry.ssr.js",
-            ),
-            qwikCityPlanModulePath: "@qwik-city-plan",
-          });
-        },
-      },
+      staticAdapter({
+        origin: "https://doganozturk.dev",
+      }),
     ],
   };
 });
